eac6d09151ca23b9e4007e2d1e6e10228cd4691b
rajthaker13 <69283649+rajthaker13@users.noreply.github.com>
2022-10-05 04:57:38 -0500

    import screens

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/SearchScreen';
+import CreatePollScreen from './screens/CreatePollScreen';
+import ActivityScreen from './screens/ActivityScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 export default function App() {
   return (
