b9f5cf1b49d561502608a238ef95b5bd27afc3ac
rajthaker13 <69283649+rajthaker13@users.noreply.github.com>
2022-10-05 04:56:33 -0500

    screens

---
+++ screens/ActivityScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+function ActivityScreen() {
+    return (
+        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+            <Text>Activity Screen - Kyle</Text>
+        </View>
+    )
+}
+export default ActivityScreen
---
+++ screens/CreatePollScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+function CreatePollScreen() {
+    return (
+        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+            <Text>Create Poll Screen - Team</Text>
+        </View>
+    )
+}
+export default CreatePollScreen
