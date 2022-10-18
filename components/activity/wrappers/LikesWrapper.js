eba11c9c5003e6ed73e5297dace9f626056849b6
rajthaker13 <69283649+rajthaker13@users.noreply.github.com>
2022-10-18 17:19:30 -0500

    likes refresh

--- components/activity/wrappers/LikesWrapper.js
+++ components/activity/wrappers/LikesWrapper.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StyleSheet, View, Text, TextInput, ScrollView, SafeAreaView, Dimensions } from 'react-native'
 import { useEffect, useState } from 'react/cjs/react.development';
 import Likes from '../Likes';
-
+import { useIsFocused } from '@react-navigation/native';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -10,17 +10,18 @@ const windowHeight = Dimensions.get('window').height;
 
 export default function LikesWrapper(props) {
     const [likesActivity, setLikesActivity] = useState([])
+    const isFocused = useIsFocused();
     useEffect(() => {
         setLikesActivity(props.likeActivity)
 
-    }, [])
+    }, [isFocused])
     return (
         <>
             <View style={{ height: windowHeight * .22 }}>
                 <Text style={{ marginLeft: 20, color: 'white', fontFamily: "Federo", fontSize: 20 }}> {props.title}</Text>
                 <SafeAreaView>
                     <ScrollView horizontal={true}>
-                        {likesActivity.map((like) => {
+                        {props.likeActivity.map((like) => {
                             return (
                                 <Likes title={like.timestamp} time="1h" answerNum="432" />
 
