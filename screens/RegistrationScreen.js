f2d1dd73214cd00b2a6577d44adbe0ff7e31ea11
rajthaker13 <69283649+rajthaker13@users.noreply.github.com>
2022-10-18 13:44:57 -0500

    registeractivity

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -40,6 +40,7 @@ export default function RegistrationScreen() {
                     description: '',
                     polls: [],
                     groups: [],
+                    activity: '',
                 })
 
                 navigator.replace("Login")
