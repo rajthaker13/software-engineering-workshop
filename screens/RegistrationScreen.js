931d3497bb6fbc3985fce260a1c7aa99ddbfe43a
rajthaker13 <69283649+rajthaker13@users.noreply.github.com>
2022-11-07 23:47:44 -0600

    register firestore

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -5,18 +5,34 @@ import { useState } from "react";
 import { Button, SafeAreaView, Text, TextInput, TouchableHighlight, View } from "react-native";
 import { COLORS } from '../components/Colors/ColorScheme'
 import { MStyles } from "../components/Mason Styles/MStyles";
+import { collection, addDoc, setDoc, doc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 
-export default function RegistrationScreen() {
+export default function RegistrationScreen(props) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
     const [firstname, setFirstname] = useState('');
     const [lastname, setLastname] = useState('');
     const navigator = useNavigation()
-    const auth = getAuth()
+    const firebaseConfig = {
+        apiKey: "AIzaSyAN3OCr7y5e7I_ba_ASonj2HoAgrnSQbYU",
+        authDomain: "pollme-24549.firebaseapp.com",
+        databaseURL: "https://pollme-24549-default-rtdb.firebaseio.com",
+        projectId: "pollme-24549",
+        storageBucket: "pollme-24549.appspot.com",
+        messagingSenderId: "517411271651",
+        appId: "1:517411271651:web:2ce5925cd5faf436eba6d6",
+        measurementId: "G-TMWX0CVP82"
+    };
 
-    const handleSignup = () => (
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth()
+    const db = getFirestore(app);
+
+    const handleSignup = async () => (
         createUserWithEmailAndPassword(auth, email, password)
             .then(userCredentials => {
                 const user = userCredentials.user;
@@ -26,11 +42,10 @@ export default function RegistrationScreen() {
                 alert('Verification email sent')
             })
             .catch(error => alert(error.message))
-            .then(() => {
-                const db = getDatabase();
-                const reference = ref(db, 'users/' + auth.currentUser.uid)
+            .then(async () => {
+                // const reference = ref(db, 'users/' + auth.currentUser.uid)
 
-                set(reference, {
+                const docRef = await setDoc(doc(db, "users", auth.currentUser.uid), {
                     username: username,
                     profile_picture_url: 'http://tny.im/tGI',
                     followers: false,
@@ -53,7 +68,7 @@ export default function RegistrationScreen() {
     );
 
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.Background}}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.Background }}>
             <Text style={MStyles.pageTitle}>User Registration</Text>
             <View style={MStyles.headerContainer}>
                 <Text style={MStyles.header}>Email</Text>
@@ -62,36 +77,36 @@ export default function RegistrationScreen() {
                 autoCapitalize={'none'}
                 value={email}
                 onChangeText={text => setEmail(text)}
-                style={MStyles.input}/>
+                style={MStyles.input} />
             <View style={MStyles.headerContainer}>
                 <Text style={MStyles.header}>Password</Text>
             </View>
             <TextInput secureTextEntry="true"
                 autoCapitalize={'none'}
                 value={password}
-                onChangeText={text => setPassword(text)} 
-                style={MStyles.input}/>
+                onChangeText={text => setPassword(text)}
+                style={MStyles.input} />
             <View style={MStyles.headerContainer}>
                 <Text style={MStyles.header}>Username</Text>
             </View>
             <TextInput value={username}
                 autoCapitalize={'none'}
-                onChangeText={text => setUsername(text)} 
-                style={MStyles.input}/>
+                onChangeText={text => setUsername(text)}
+                style={MStyles.input} />
             <View style={MStyles.headerContainer}>
                 <Text style={MStyles.header}>First Name</Text>
             </View>
             <TextInput value={firstname}
                 autoCapitalize={'none'}
-                onChangeText={text => setFirstname(text)} 
-                style={MStyles.input}/>
+                onChangeText={text => setFirstname(text)}
+                style={MStyles.input} />
             <View style={MStyles.headerContainer}>
                 <Text style={MStyles.header}>Last Name</Text>
             </View>
             <TextInput value={lastname}
                 autoCapitalize={'none'}
-                onChangeText={text => setLastname(text)} 
-                style={MStyles.input}/>
+                onChangeText={text => setLastname(text)}
+                style={MStyles.input} />
             <TouchableHighlight style={MStyles.buttonSolidBackground} onPress={handleSignup}>
                 <Text style={MStyles.buttonSolidBackgroundText}>Sign Up</Text>
             </TouchableHighlight>
