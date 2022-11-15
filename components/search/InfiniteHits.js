a175232df7d50de81516d4ee1db2044d69f45e21
stsadmin <stsadmin@STSDEV2202.lan>
2022-11-15 14:00:46 -0600

    search bar modal done

--- components/search/InfiniteHits.js
+++ components/search/InfiniteHits.js
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types';
 import { connectInfiniteHits } from 'react-instantsearch-native';
 import Highlight from './Highlight';
 import { COLORS } from '../Colors/ColorScheme';
-import PollModal from '../common/PollModal';
+import PollModal from '../common/PollModalForSearch';
 import { useEffect, useReducer, useState } from 'react';
+import { MStyles } from '../Mason Styles/MStyles';
+import { TouchableOpacity } from 'react-native';
 
 
 const styles = StyleSheet.create({
@@ -32,22 +34,25 @@ const InfiniteHits = ({ hits, hasMore, refineNext }) => (
     ItemSeparatorComponent={() => <View style={styles.separator} />}
     onEndReached={() => hasMore && refineNext()}
     renderItem={({ item }) => (
-      <View style={styles.item}>
-        {/* <Text style={styles.titleText}>
-          <Highlight attribute="{{attributesToDisplay.[0]}}" hit={item} />
-        </Text>
-        <Text>
-          <Highlight attribute="{{this}}" hit={item} />
-        </Text> */}
-        <Pressable onPress={()=>
-          {console.log("xxx", item.objectID)}
-          // setModalVisible(true)
-        }>
-          {/* <PollModal pollID={item.objectID}  setVisibility={props.modalVisible}/> */}
-          <Text style={{color: COLORS.Paragraph}}>{JSON.stringify(item.title).slice(0, 100)}</Text>
-          {/* {console.log("xxx", item.objectID)} */}
-        </Pressable>
-      </View>
+
+      <TouchableOpacity style={styles.item}>
+          <PollModal pollID={item.objectID}/>  
+      </TouchableOpacity>
+
+      // <PollModal pollID={item.objectID}/>
+
+      // <View style={styles.item}>
+      //   {/* <Text style={styles.titleText}>
+      //     <Highlight attribute="{{attributesToDisplay.[0]}}" hit={item} />
+      //   </Text>
+      //   <Text>
+      //     <Highlight attribute="{{this}}" hit={item} />
+      //   </Text> */}
+      //   <Pressable onPress={()=> {console.log("xxx", item.objectID)}}>
+      //     <Text style={{color: COLORS.Paragraph}}>{JSON.stringify(item.title).slice(0, 100)}</Text>
+      //     {/* {console.log("xxx", item.objectID)} */}
+      //   </Pressable>
+      // </View>
     )}
   />
 );
